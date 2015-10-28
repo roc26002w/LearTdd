de8c1a09c47606e83348a5a2b464277100c48b28
UrAD-Rocko <rocko@urad.com.tw>
2015-10-29 01:59:15 +0800

    # Test 6 *一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370 *test_Buy_v1Book_one_v2Book_one_v3Book_three_Get_370_OK

--- src/BooksDiscountCalculator.php
+++ src/BooksDiscountCalculator.php
@@ -9,6 +9,9 @@ class BooksDiscountCalculator
 
     protected $books;
     protected $total;
+    protected $bookCount;
+    protected $unBooks;
+    protected $totalList;
 
     public function addBook(Book $book)
     {
@@ -19,8 +22,23 @@ class BooksDiscountCalculator
     {
         $this->total = 0;
         foreach ($this->books as $book) {
-            $this->total += $book->getPrice();
+            $bookCount = $book->getCount();
+            for($i = 0;$i < $bookCount ; $i++){
+                
+                if(!isset($this->unBooks[$i])){
+                    $this->unBooks[$i] = [];
+                }
+                if(!isset($this->totalList[$i])){
+                    $this->totalList[$i] = 0;
+                }
+                
+                if(!in_array($book->getTitle(),$this->unBooks[$i])){
+                    $this->unBooks[$i][]    =   $book->getTitle();
+                }
+                $this->totalList[$i] += $book->getPrice();
+            }
         }
+        
         $this->getBookDiscount();
 
     }
@@ -32,18 +50,34 @@ class BooksDiscountCalculator
 
     protected function getBookDiscount()
     {
-        switch(count($this->books)){
+
+        foreach ($this->unBooks as $key => $groupBook)
+        {
+            $groupCount = count($this->unBooks[$key]);
+            $this->total += $this->totalList[$key] * $this->getDiscountValue($groupCount);
+        }
+
+    }
+
+    protected function getDiscountValue($groupCount)
+    {
+
+        switch ($groupCount)
+        {
             case 2:
-                $this->total = $this->total * 0.95;
+                return 0.95;
                 break;
             case 3:
-                $this->total = $this->total * 0.9;
+                return 0.9;
                 break;
             case 4:
-                $this->total = $this->total * 0.8;
+                return 0.8;
                 break;
             case 5:
-                $this->total = $this->total * 0.75;
+                return 0.75;
+                break;
+            default:
+                return 1;
                 break;
         }
     }
